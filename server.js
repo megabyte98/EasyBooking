06dec39aacba7f878127a866b38f5bf5a0572d7c
Manvendra Bansal <bansalmanvendra@gmail.com>
2021-07-05 18:39:25 +0530

    .

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use("/api/hall", require("./routes/hall"))
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
+  const path = require("path")
+  app.get("*",(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
 }
 
 app.listen(PORT, (err) => {
